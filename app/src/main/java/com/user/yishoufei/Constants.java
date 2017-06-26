f346985a8ee3dded379953956a8547db25082ceb
hzlinj <hzlinj@126.com>
2017-06-26 18:07:56 +0800

    添加开屏广告debug

---
+++ app/src/main/java/com/user/yishoufei/Constants.java
@@ -0,0 +1,17 @@
+package com.user.yishoufei;
+
+/**
+ * Created by User on 2017/6/26.
+ */
+
+public class Constants {
+    public static final String APPID = "1106242054";
+    public static final String BannerPosID = "9079537218417626401";
+    public static final String APPWallPosID = "9007479624379698465";
+    public static final String InterteristalPosID = "8575134060152130849";
+    public static final String SplashPosID = "8863364436303842593";
+    public static final String GridAppWallPosID = "9007479624379698465";
+    public static final String NativePosID = "5000709048439488";
+    public static final String NativeVideoPosID = "2050206699818455";
+
+}
